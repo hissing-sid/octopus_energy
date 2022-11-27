b74d063ffb578263e158d4d959ab88e55881d786
hissing-sid <86068841+hissing-sid@users.noreply.github.com>
2022-11-27 19:59:44 +0000

    added logging

--- octopus.py
+++ octopus.py
@@ -1,8 +1,10 @@
-import datetime, configparser, json, requests, socket, time
+import datetime, configparser, json, requests, socket, time, logging
+from datetime import timezone
 from dateutil import parser
 from tqdm import tqdm
 
 def get_account_details(accountID, key):
+    print('\n\nImporting account details :', accountID, '\n')
 
     account_api_url = 'https://api.octopus.energy/v1/accounts/' + accountID
     account = requests.get(account_api_url, auth = (key,''))
@@ -23,58 +25,36 @@ def get_account_details(accountID, key):
     }
 
 def retrieve_rates(api_key, accountID, account_details):
+    
+    print('Retrieving tariffs...')
+    tariffs = []
     for fuel in {'gas','electricity'}:
         # retrieve all applicable tariffs and get product rates
         for tariff in account_details[fuel]['tariffs']:
+
             tariff_code=tariff['tariff_code']
-            tariff_from = normalise_date(tariff['valid_from'])
-            tariff_to = normalise_date(tariff['valid_to'])
 
-            
-            
+            tariff_from = standardise_date(tariff['valid_from'])
+            tariff_to = standardise_date(tariff['valid_to'])
+           
             product_code=tariff_code.removesuffix('-E')[5:]
-            
+            print('From: %s  To: %s in UTC: %s' %(tariff_from, tariff_to, 'x'))
 
-    exit()
 
-def normalise_date(date):
+def standardise_date(date):
+    result = date
     if date is None:
-        print('None')
-        result = datetime.datetime.now()
-    elif "00Z" in date:
-        print('Z')
-        result =  datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:00Z")
-    elif "+" in date:
-        print('No Z')
-        # result = datetime.datetime.strptime(date.replace('+01:00', 'Z'), "%Y-%m-%dT%H:%M:00Z" ) + datetime.timedelta(hours=1)
-        result = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:00%z" ) 
+        result = datetime.datetime.utcnow()
 
-    print(date, '=>', result)
     return result
-
-def convert_to_epoch(date):
-    if date is None:
-        epoch = datetime.datetime.now().timestamp()
-    elif "00Z" in date:
-        epoch =  datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:00Z").timestamp()
-    else:
-        date_adjusted = datetime.datetime.strptime(date.replace('+01:00', 'Z'), "%Y-%m-%dT%H:%M:00Z" ) + datetime.timedelta(hours=1)
-        epoch =  date_adjusted.timestamp()
-
-    return epoch
 
 def get_current_tariff(api_key, account_details, fuel, epoch):
     for tariff in account_details[fuel]['tariffs']:
 
-        tariff_from = convert_to_epoch(tariff['valid_from'])
-        tariff_to = convert_to_epoch(tariff['valid_to'])
+        tariff_from = parser.parse(standardise_date(tariff['valid_from']))
+        tariff_to = parser.parse(standardise_date(tariff['valid_to']))
 
-        try:
-            tariff_to = datetime.datetime.strptime(tariff['valid_to'], "%Y-%m-%dT%H:%M:00Z").timestamp()
-        except:
-            tariff_to = datetime.datetime.now().timestamp()
-
-        if  (epoch >= tariff_from ) and (epoch <= tariff_to):
+        if  (epoch >= tariff_from.timestamp() ) and (epoch <= tariff_to.timestamp()):
             tariff_code = tariff['tariff_code']
 
     # Strip first 5 chars (e.g. G-1R-) and remove any trailing -E to get the product code.
@@ -84,14 +64,6 @@ def get_current_tariff(api_key, account_details, fuel, epoch):
     response = requests.get(account_api_url, auth = (api_key,''))
     product = response.json()
 
-    print('\nTariff Code:', tariff_code)
-    print('Product:', product_code)
-    print('Fuel:', fuel)
-    print(product['description'])
-    print('===========================================================================')
-    print(product)
-    exit()
-
     # for GO electricity product, standard_unit_rates show the day / night rates NOT the /(day or night)-unit-rates/ APIs
     # https://api.octopus.energy/v1/products/%s/%s-tariffs/%s/standard-unit-rates/ % (product_code, fuel, tariff-code)
     # https://api.octopus.energy/v1/products/%s/%s-tariffs/%s/standing-charges/ % (product_code, fuel, tariff-code)
@@ -100,15 +72,19 @@ def get_current_tariff(api_key, account_details, fuel, epoch):
 
 if __name__=="__main__":
 
-
     config = configparser.ConfigParser()
     config.read('config.ini')
+
     api_key = config['account']['api_key']
+    api_url = config['account']['api_url']
+    account_no = config['account']['number']
 
-    account_details = get_account_details(config['account']['number'],api_key)
-    rates = retrieve_rates(config['account']['number'],api_key, account_details)
+    logging.basicConfig(filename=config['logger']['log_file'], encoding='utf-8', level=logging.INFO)
 
+    account_details = get_account_details(account_no,api_key)
+    
 
+    # rates = retrieve_rates(account_no, api_key, account_details)
 
     days_to_import = int(config['import']['days'])
 
@@ -123,14 +99,16 @@ if __name__=="__main__":
     last_successful_date = 'never'
 
     # Open the connection to the carbon server
-    sock = socket.socket()
-    sock.connect((carbon_server, carbon_port))
-
-    print("\nImporting consumption data for account: %s" % config['account']['number'], end="\n\n")
+    carbon = socket.socket()
+    try:
+        carbon.connect((carbon_server, carbon_port))
+    except:
+        print('\nUnable to connect to the Carbon server at: %s port %s' % (carbon_server, carbon_port))
+        exit()
 
     for i in tqdm (range (days_to_import ), desc="Loading consumption data..."):
         # Import the data for a single day - lazy as I dont need to deal with pagination. Optimisations will come later 
-        params = {'order_by': 'period', 'period_from': import_start_date.strftime("%Y-%m-%dT%H:%M:00Z"), 'period_to': import_start_date.strftime("%Y-%m-%dT23:59:00Z") }
+        params = {'order_by': 'period', 'period_from': import_start_date.strftime("%Y-%m-%dT00:00:00Z"), 'period_to': import_start_date.strftime("%Y-%m-%dT23:59:00Z") }
         for fuel in {'gas','electricity'}:
             api_url = 'https://api.octopus.energy/v1/%s-meter-points/%s/meters/%s/consumption' % (fuel, account_details[fuel]['meter'], account_details[fuel]['serial'] )
 
@@ -138,23 +116,28 @@ if __name__=="__main__":
             response_results = response.json()
 
             if response_results['count'] != 0:
+
+                logging.info((import_start_date.strftime("%Y-%m-%dT00:00:00Z"), import_start_date.strftime("%Y-%m-%dT23:59:00Z"), response_results['count']))
+                
                 for result in response_results['results']:
                     
+                    current_interval = parser.parse(result['interval_start'])
 
-                    epoch = datetime.datetime.timestamp(parser.parse(result['interval_start'])) 
+                    epoch = datetime.datetime.timestamp(current_interval) 
                     value = result['consumption']
 
-                    x = get_current_tariff(api_key, account_details, fuel, epoch)
-            
+                    #x = get_current_tariff(api_key, account_details, epoch, fuel, epoch)
+
                     message = 'octopus.%s.consumption %s %d\n' % (fuel, value, epoch)
                     try:
-                        sock.sendto(message.encode('utf-8'), (carbon_server, carbon_port))
+                        carbon.send(message.encode('utf-8'))
+                        last_successful_date = import_start_date.strftime("%Y-%m-%d")
                     except:
                         print('Send to Carbon failed')
-                    last_successful_date = import_start_date.strftime("%Y-%m-%d")
             
-            import_start_date += delta
+        import_start_date += delta
 
     # Close the connection to the carbon server
-    sock.close()
-    print('\nImported data to at least: %s \n' % last_successful_date)
+    carbon.close()
+
+    print('\nImported data to: %s \n' % last_successful_date)
